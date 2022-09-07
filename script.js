c04b9fa8a64b57a5e13670dce5b3322acbc49b85
ethem <88254285+ethemtion@users.noreply.github.com>
2022-09-07 16:58:11 +0300

    basic github implement

--- script.js
+++ script.js
@@ -1,33 +1,49 @@
-
-let ppUrl = "https://api.github.com/users/ethemtion"
-let url = "https://api.github.com/users/ethemtion/repos"
+let ppUrl = "https://api.github.com/users/ethemtion";
+let url = "https://api.github.com/users/ethemtion/repos";
 
 $(document).ready(function () {
-    console.log("burdayım")
-    getPP()
-    getData()
-
+  getPP();
+  getData();
 });
 
 async function getPP() {
-    const response = await fetch(ppUrl);
-     const data = await response.json();
-    let avatar = data.avatar_url;
-    $("#avatar").attr("src",avatar)
-  }
+  const response = await fetch(ppUrl);
+  const data = await response.json();
+  let avatar = data.avatar_url;
+  $("#avatar").attr("src", avatar);
+}
 
-let repoName
-async function getData(){
- 
- const response = await fetch(url);
- const data = await response.json();
- console.log(data)
+let repoName, repoUrl, repoLastUpdated;
+let day,month,year;
+async function getData() {
+  const response = await fetch(url);
+  const data = await response.json();
 
-  data.forEach(repo => {
-    repoName = repo.name
+  data.reverse().forEach((repo) => {
+    repoName = repo.name;
+    repoUrl = repo.html_url;
+    repoLastUpdated = repo.updated_at;
+    const date = new Date(repoLastUpdated);
+    day = date.getDate()
+    month = date.getMonth()
+    year = date.getFullYear()
   });
 
-//   TEST
-  $("#profile-tab").html(repoName)
-  $("profile-tab").html()
+ 
+
+  //   TEST
+  $("#profile-tab").html(repoName);
+  $("#profile-tab-pane").html(`
+  <div class="tabContent text-center">
+                <p class="pt-3 text-end">Last updated: ${day}.${month}.${year}</p>
+                <a href="${repoUrl}" class="btn btn-primary m-2 w-25 text-wrap mx-auto" target="_blank">${repoName} Github </a>
+                  <p class="pt-3">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Voluptatibus aliquam, inventore sint suscipit officia odit
+                    ad, non dolore voluptate facere repellat sapiente id,
+                    accusantium illo ipsam praesentium distinctio et dolorem.
+                  </p>
+
+                </div>
+                `);
 }
