795942a4024dc544f4d0ca75f8f95668c7667a5d
ethem <88254285+ethemtion@users.noreply.github.com>
2022-09-08 16:58:06 +0300

    full implement

--- script.js
+++ script.js
@@ -14,33 +14,34 @@ async function getPP() {
 }
 
 let repoName, repoUrl, repoLastUpdated, repoDesc;
-let day,month,year;
+let day, month, year;
+let counter = -1;
 async function getData() {
   const response = await fetch(url);
   const data = await response.json();
 
-
-console.log(data)
-
+  console.log(data);
 
   data.reverse().forEach((repo) => {
     repoName = repo.name;
     repoUrl = repo.html_url;
     repoLastUpdated = repo.updated_at;
     const date = new Date(repoLastUpdated);
-    day = date.getDate()
-    month = date.getMonth()
-    year = date.getFullYear()
+    day = date.getDate();
+    month = date.getMonth();
+    year = date.getFullYear();
     repoDesc = repo.description;
+    makeTab();
+    makeTabContent();
   });
-
- 
 
   //   TEST
   $("#profile-tab").html(repoName);
   $("#profile-tab-pane").html(`
   <div class="tabContent text-center">
-                <p class="pt-3 text-end">Last updated: ${day}.${month + 1}.${year}</p>
+                <p class="pt-3 text-end">Last updated: ${day}.${
+    month + 1
+  }.${year}</p>
                 <a href="${repoUrl}" class="btn btn-primary m-2 w-25 text-wrap mx-auto" target="_blank">${repoName} Github </a>
                   <p class="pt-3">
                     ${repoDesc}
@@ -48,4 +49,27 @@ console.log(data)
 
                 </div>
                 `);
+}
+
+function makeTab() {
+  let html = `<li class="nav-item`;
+  if (++counter == 0) html += ` ms-5`;
+  html += `" role="presentation"><button
+  class="nav-link id="${repoName}-tab" data-bs-toggle="tab" data-bs-target="#${repoName}-tab-pane" type="button" role="tab" aria-controls="${repoName}-tab-pane" aria-selected="false">${repoName}</button></li>`;
+
+  $(".nav").append(html);
+}
+
+function makeTabContent() {
+  let html = `<div class="tab-pane fade" id="${repoName}-tab-pane" role="tabpanel" aria-labelledby="${repoName}-tab" tabindex="0">`;
+  html += `<div class="tabContent text-center">
+  <p class="pt-3 text-end">Last updated: ${day}.${month + 1}.${year}</p>
+  <a href="${repoUrl}" class="btn btn-primary m-2 w-25 text-wrap mx-auto" target="_blank">${repoName} Github </a>
+    <p class="pt-3">
+      ${repoDesc}
+    </p>
+  </div>
+  `;
+
+  $(".tab-content").append(html)
 }
